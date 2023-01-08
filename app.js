9fcda070cf8c0de0ef8af0aa50c6cae945d3799e
Vinicius Santana <santanaviny@gmail.com>
2023-01-08 14:27:13 -0800

    Simple bot setup

---
+++ app.js
@@ -0,0 +1,31 @@
+const CronJob = require('cron').CronJob;
+const famousQuotes = require('./famousQuotes');
+const { postTweet } = require('./bot');
+
+/**
+ * @description Get Random Quote from our list
+ * @author im-chandan
+ * @returns {*} String 
+ */
+function getQuotes(){
+    return famousQuotes.QUOTES[Math.floor(Math.random()*famousQuotes.QUOTES.length)];
+}
+
+//Set CronJob to run our BOT everyday @ Morning 7 AM
+new CronJob(
+	// '0 7 * * *', // everyday at 7 am
+	'* * * * * *', // everyday at 7 am
+	function() {
+		// Call Twitter BOT to post new Tweet
+        try{
+          postTweet(getQuotes());
+        } catch(err) {
+          console.log("error", err)
+        }
+	},
+	null,
+	true,
+	'Asia/Kolkata'
+);
+
+// You can directly call postTweet() function to post new Tweet any time
--- readme.md
+++ readme.md
@@ -1,1 +1,13 @@
 # Twtitter AI Bot
+
+ref: https://www.linkedin.com/pulse/how-create-twitter-bot-nodejs-chandan-thakur/?trk=articles_directory
+
+
+```bash
+.env
+
+TWITTER_API_KEY="xxxx"
+TWITTER_API_SECRET="xxxx"
+TWITTER_ACCESS_TOKEN="xxxx"
+TWITTER_ACCESS_TOKEN_SECRET="xxxx"
+```
